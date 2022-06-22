6b7dd2009096208f269bac1a185a8bb53f57c824
AndreLPD <andre_luis_pessoa@hotmail.com>
2022-06-21 21:21:52 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# My First Component Vue 3 + Vite
+
+This is my first component with Vue 3
+
+## Recommended IDE Setup
+
+- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)
---
+++ src/components/Counter.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0);
+
+  defineProps({
+  msg: String,
+  initialValue:Number
+})
+
+function increment(){
+  count.value++
+}
+
+function decrement(){
+  count.value--
+}
+
+function initialValue(){
+  count.value = 0;
+}
+
+
+
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    {{count}}
+  </p>
+
+  <p>
+    <a>
+      <button type="button" @click="increment">increment</button>
+    </a>
+    <a>
+      <button type="button" @click="decrement">decrement</button>
+    </a>
+    <a>
+      <button type="button" @click="initialValue">initial value</button>
+    </a>
+  </p>
+
+  
+  
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
